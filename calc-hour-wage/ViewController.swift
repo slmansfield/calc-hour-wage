e76ed332d1f5f1f19898b92507154bdd95880849
Shawn Mansfield <shawn.mansfield@gmail.com>
2020-05-11 17:27:10 -0400

    Initial Commit

---
+++ calc-hour-wage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc-hour-wage
+//
+//  Created by Shawn Mansfield on 5/11/20.
+//  Copyright © 2020 Shawn Mansfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
